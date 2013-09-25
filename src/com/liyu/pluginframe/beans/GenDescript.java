c5da6c3ea4e3326adf553dbd2e267d8f9a6326cd
WangJian <wangjian2254@gmail.com>
2013-09-25 17:42:50 +0800

    添加plugin.xml

---
+++ src/com/liyu/pluginframe/beans/GenDescript.java
@@ -0,0 +1,31 @@
+package com.liyu.pluginframe.beans;
+
+public class GenDescript {
+	protected int iconResId;
+	
+	protected String subTitle;
+	
+	protected String description;
+	
+	public GenDescript() {
+	}
+
+	public int getIconResId() {
+		return iconResId;
+	}
+
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	
+	
+}
